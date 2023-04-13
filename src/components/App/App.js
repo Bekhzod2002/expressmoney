c8515755a8201354b8a11624dac76064a4ffeb9a
unknown <oakhmadjonov1@gmail.com>
2023-04-13 15:38:24 +0500

    Everythingg is ready to start

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,7 +5,9 @@ function App() {
     <div className="App">
       <div className="home">
         <div className="home__container">
-          <div className="home__wrapper"></div>
+          <div className="home__wrapper">
+            <h1>Hello Express Money</h1>
+          </div>
         </div>
       </div>
     </div>
