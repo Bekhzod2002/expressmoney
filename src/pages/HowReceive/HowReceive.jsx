d87ae7271f0e49616b1dc8a7854b616984409d76
unknown <oakhmadjonov1@gmail.com>
2023-04-13 21:52:59 +0500

    Как получить started some chnages added

---
+++ src/pages/HowReceive/HowReceive.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaChevronRight } from "react-icons/fa";
+import "./HowReceive.scss";
+import SelectAmount from "../../components/SelectAmount/SelectAmount";
+
+const HowReceive = () => {
+  return (
+    <div className="howreceive">
+      <div className="howreceive__container">
+        <div className="howreceive__wrapper">
+          <div className="howreceive__wrapper top-path">
+            <div className="top_path_links">
+              <Link className="top_path_links-first-step" to="/">
+                Главная{" "}
+              </Link>
+              <span>
+                <FaChevronRight />
+              </span>
+              <Link className="top_path_links-second-step" to="/how-to-receive">
+                Как получить{" "}
+              </Link>
+            </div>
+          </div>
+          {/* Top-Path End */}
+          <div className="howreceive__content">
+            <div className="description"></div>
+            <div className="select__amount_comp">
+              <SelectAmount />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowReceive;
