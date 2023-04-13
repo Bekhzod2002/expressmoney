6eb79d667a71f816e4a0791180c21758807a19ba
unknown <oakhmadjonov1@gmail.com>
2023-04-13 15:27:22 +0500

    File structure added

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home">
+        <div className="home__container">
+          <div className="home__wrapper"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
