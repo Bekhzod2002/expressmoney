fe19da31af14d3adb215a6959ed00c2048e0f44a
unknown <oakhmadjonov1@gmail.com>
2023-04-13 16:38:01 +0500

    Why it is not added

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { BsArrowRightCircle } from "react-icons/bs";
+import "./Button.scss";
 const Button = () => {
-  return <div>Button</div>;
+  const default__btn__content = "Оформить заявку";
+  return (
+    <Link className="default__btn-container">
+      <button className="default__btn">
+        {default__btn__content}
+        <BsArrowRightCircle />
+        {/* <span className="default__btn-icon"></span> */}
+      </button>
+    </Link>
+  );
 };
 
 export default Button;
